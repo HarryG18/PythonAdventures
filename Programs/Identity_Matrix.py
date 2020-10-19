923f6069a35495ee2884b39c444504e80fbbe66c
Harry Grace <you@example.com>
2020-10-19 19:58:12 +0100

    Upload

--- Programs/Identity_Matrix.py
+++ Programs/Identity_Matrix.py
@@ -3,5 +3,5 @@ def id_mtrx(n):
     # if type(n) is not int:
     #     return "Error"
     return "Error" if type(n) is not int else np.identity(n, dtype=int).tolist() if n > 0 else np.fliplr(np.identity(-n, dtype= int)).tolist()
-
+#
 print(id_mtrx(2))
